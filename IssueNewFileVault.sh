d2f8436c25f47722cecec1c7ca9e12f7ae702c92
killianz26 <55172706+killianz26@users.noreply.github.com>
2021-10-23 21:45:44 -0400

    Update IssueNewFileVault.sh

--- IssueNewFileVault.sh
+++ IssueNewFileVault.sh
@@ -1,2 +1,3 @@
-sudo fdesetup changerecovery -personal
-sudo jamf recon
+sudo ln -sf /etc/letsencrypt/live/bg-cloud.pro-0001/fullchain.pem /opt/bitnami/apache2/conf/bitnami/certs/server.crt
+$ sudo ln -sf /etc/letsencrypt/live/bg-cloud.pro-0001/privkey.pem /opt/bitnami/apache2/conf/bitnami/certs/server.key
+test
